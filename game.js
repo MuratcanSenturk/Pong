4b0e0c83fad5b9dee9e33558db10618e3945bb31
Muratcan Şentürk <muratcansenturk2000@hotmail.com>
2019-03-03 21:17:45 +0300

    avaible mobile support
    pong game is now supporting mobile devices to play.

--- game.js
+++ game.js
@@ -3,14 +3,12 @@
     var draw = canvas.getContext("2d");
     document.addEventListener("keydown", this.onKeyPress.bind());
 
-    var cnvHeight = document.getElementById("myCanvas").height;
-    var cnvWidth = document.getElementById("myCanvas").width;
     var player1X =  50;
-    var player1Y = 200;
+    var player1Y = 240;
     var cpuX = 750;
-    var cpuY = 200;
-    var objX = cnvWidth/2;
-    var objY = cnvHeight/2;
+    var cpuY = 240;
+    var objX = canvas.width/2;
+    var objY = canvas.height/2;
     var velocityX;
     var velocityY;
     var player1Scr = 0;
@@ -18,7 +16,6 @@
     var interval1;
     var interval2;
     var progSpeed;
-
 
     function start(difficulty){
         switch(difficulty){
@@ -38,22 +35,33 @@
         velocityY = 0;
         game();
         interval1 = setInterval(game, 1);
-        document.getElementById("enter").innerHTML = "Press Enter to Start"
+        document.getElementById("enterDesktop").innerHTML = "Press Enter to Start";
+        document.getElementById("enterMobile").innerHTML = "Press Here to Start"
     }
     function goHome(){
         clearInterval(interval1);
         clearInterval(interval2);
+        player1Scr = 0;
+        player2Scr = 0;
+        velocityX = 1;
+        velocityY = 1;
+        objX = canvas.width/2;
+        objY = canvas.height/2;
+        player1X =  50;
+        player1Y = 200;
+        cpuX = 750;
+        cpuY = 200;
         document.getElementById("homePage").style.display = "block";
         document.getElementById("gamePage").style.display = "none";
     }
     function clear(){
         draw.fillStyle = "white";
-        draw.fillRect(0, 0, cnvWidth, cnvHeight);
+        draw.fillRect(0, 0, canvas.width, canvas.height);
     }
     function ai(){
         if(objY - 50 < cpuY && cpuY != 0){
             cpuY -= 5;
-        }else if(objY - 50 > cpuY && cpuY != cnvHeight - 100){
+        }else if(objY - 50 > cpuY && cpuY != canvas.height - 100){
             cpuY += 5;
         }
     }
@@ -81,40 +89,61 @@
         }
         //Player1 Scored
         if(objX == 810){
-            objX = cnvWidth/2;
-            objY = cnvHeight/2;
+            objX = canvas.width/2;
+            objY = canvas.height/2;
             velocityX = 1;
             velocityY = 1;
             player1Scr++;
         }
         //Computer Scored
         if(objX == -10){
-            objX = cnvWidth/2;
-            objY = cnvHeight/2;
+            objX = canvas.width/2;
+            objY = canvas.height/2;
             player2Scr++;
         }
         
         document.getElementById("player1Score").innerHTML = player1Scr;
         document.getElementById("player2Score").innerHTML = player2Scr;
     }
+    //Mobile Controls
+    function mobileControl(direction){
+        if(direction == "left" && player1Y != 0){
+            player1Y -= 20;
+        }
+        if(direction == "right" && player1Y != canvas.height - 60){
+            player1Y += 20;
+        }
+    }
+    function mobileStart(){
+        velocityX = 1;
+        velocityY = 1;
+        objX = canvas.width/2;
+        objY = canvas.height/2;
+        player1X =  50;
+        player1Y = 200;
+        cpuX = 750;
+        cpuY = 200;
+        document.getElementById("enterMobile").innerHTML = "";
+        interval2 = setInterval(ai, progSpeed);
+    }
     function onKeyPress(e){
         if(e.keyCode == 13){
             velocityX = 1;
             velocityY = 1;
-            objX = cnvWidth/2;
-            objY = cnvHeight/2;
+            objX = canvas.width/2;
+            objY = canvas.height/2;
             player1X =  50;
             player1Y = 200;
             cpuX = 750;
             cpuY = 200;
-            document.getElementById("enter").innerHTML = "";
+            document.getElementById("enterDesktop").innerHTML = "";
             interval2 = setInterval(ai, progSpeed);
 
         }
         if(e.keyCode == 38 && player1Y != 0){
             player1Y -= 20;
         }
-        if(e.keyCode == 40 && player1Y != cnvHeight - 100){
+        if(e.keyCode == 40 && player1Y != canvas.height - 60){
             player1Y += 20;
         }
     }
